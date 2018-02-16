849174e64afab1b7224292a56d3c391bcf45fec8
Tobias Ask <tobiaas@stud.ntnu.no>
2018-02-16 21:56:18 +0100

    Fix height of selection bar, and add support for messages. A style bug is still present, though.

--- sb4e/src/no/tobask/sb4e/EditorWindowController.java
+++ sb4e/src/no/tobask/sb4e/EditorWindowController.java
@@ -1,6 +1,7 @@
 package no.tobask.sb4e;
 
 import com.oracle.javafx.scenebuilder.app.info.InfoPanelController;
+import com.oracle.javafx.scenebuilder.app.message.MessageBarController;
 import com.oracle.javafx.scenebuilder.app.selectionbar.SelectionBarController;
 import com.oracle.javafx.scenebuilder.kit.editor.EditorController;
 import com.oracle.javafx.scenebuilder.kit.editor.i18n.I18N;
@@ -24,13 +25,14 @@ public class EditorWindowController extends AbstractFxmlWindowController {
 	HierarchyPanelController hierarchyPanelController;
 	InfoPanelController infoPanelController;
 	SearchController librarySearchController;
+	MessageBarController messageBarController;
 	
 	@FXML
 	StackPane libraryPanelHost;
 	@FXML
 	StackPane contentPanelHost;
 	@FXML
-	StackPane selectionBarHost;
+	StackPane messageBarHost;
 	@FXML
 	StackPane hierarchyPanelHost;
 	@FXML
@@ -47,16 +49,20 @@ public class EditorWindowController extends AbstractFxmlWindowController {
 		hierarchyPanelController = new HierarchyPanelController(editorController);
 		infoPanelController = new InfoPanelController(editorController);
 		librarySearchController = new SearchController(editorController);
+		messageBarController = new MessageBarController(editorController);
 	}
 	
 	@Override
 	protected void controllerDidLoadFxml() {
 		libraryPanelHost.getChildren().add(libraryPanelController.getPanelRoot());
 		contentPanelHost.getChildren().add(contentPanelController.getPanelRoot());
-		selectionBarHost.getChildren().add(selectionBarController.getPanelRoot());
 		hierarchyPanelHost.getChildren().add(hierarchyPanelController.getPanelRoot());
 		infoPanelHost.getChildren().add(infoPanelController.getPanelRoot());
 		librarySearchPanelHost.getChildren().add(librarySearchController.getPanelRoot());
+		
+        messageBarHost.getChildren().add(messageBarController.getPanelRoot());
+        messageBarController.getSelectionBarHost().getChildren().add(
+                selectionBarController.getPanelRoot());
 		
 		librarySearchController.textProperty().addListener((ov, oldStr, newStr) ->
 			libraryPanelController.setSearchPattern(newStr));
