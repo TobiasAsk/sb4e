c732babc6fb88f0cb081ee9a1ddc30edaef04a64
Tobias Ask <tobiaas@stud.ntnu.no>
2018-02-26 18:31:53 +0100

    Add support for cut, copy and paste.

--- sb4e/src/no/tobask/sb4e/editors/FXMLEditorContributor.java
+++ sb4e/src/no/tobask/sb4e/editors/FXMLEditorContributor.java
@@ -1,22 +1,26 @@
 package no.tobask.sb4e.editors;
 
+import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.actions.ActionFactory;
 import org.eclipse.ui.part.EditorActionBarContributor;
 
 public class FXMLEditorContributor extends EditorActionBarContributor {
-
+	
 	public FXMLEditorContributor() {
-		// TODO Auto-generated constructor stub
 	}
 	
 	@Override
 	public void setActiveEditor(IEditorPart targetEditor) {
 		if (targetEditor instanceof FXMLEditor) {
 			FXMLEditor editor = (FXMLEditor) targetEditor;
-			getActionBars().setGlobalActionHandler(ActionFactory.UNDO.getId(), editor.getUndoActionHandler());
-			getActionBars().setGlobalActionHandler(ActionFactory.REDO.getId(), editor.getRedoActionHandler());
-			getActionBars().updateActionBars();
+			IActionBars actionBars = getActionBars();
+			actionBars.setGlobalActionHandler(ActionFactory.UNDO.getId(), editor.getUndoActionHandler());
+			actionBars.setGlobalActionHandler(ActionFactory.REDO.getId(), editor.getRedoActionHandler());
+			actionBars.setGlobalActionHandler(ActionFactory.COPY.getId(), editor.getCopyHandler());
+			actionBars.setGlobalActionHandler(ActionFactory.CUT.getId(), editor.getCutHandler());
+			actionBars.setGlobalActionHandler(ActionFactory.PASTE.getId(), editor.getPasteHandler());
+			actionBars.updateActionBars();
 		}
 	}
 
